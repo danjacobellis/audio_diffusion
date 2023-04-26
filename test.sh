742b1a7e4190dd77516957fb6afc2c26fe93b2a1
Dan Jacobellis <danjacobellis@utexas.edu>
2023-04-26 09:13:49 -0500

    first successful train

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export OUTPUT_DIR="/sddata/finetune/lora/pokemon"
+export DATASET_NAME="danjacobellis/test"
+
+accelerate launch --mixed_precision="fp16"  train_text_to_image_lora.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --dataset_name=$DATASET_NAME \
+  --dataloader_num_workers=8 \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=15000 \
+  --learning_rate=1e-04 \
+  --max_grad_norm=1 \
+  --lr_scheduler="cosine" --lr_warmup_steps=0 \
+  --output_dir=${OUTPUT_DIR} \
+  --report_to=wandb \
+  --checkpointing_steps=500 \
+  --validation_prompt="Guitar" \
+  --seed=1337
